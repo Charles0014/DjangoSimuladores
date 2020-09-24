8171019b29811d02651555909afd234689c42e06
Charles0014 <ccharlesroberto@gmail.com>
2020-09-24 17:21:48 -0300

    Ajuste no run -- executar com python2

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 kill -9 $(lsof -t -i:9008) >/dev/null 2>&1
-python manage.py runserver 0.0.0.0:9008 >/dev/null 2>&1 & disown
+python2 manage.py runserver 0.0.0.0:9008 >/dev/null 2>&1 & disown
 echo -e "http://localhost:9008"
 
